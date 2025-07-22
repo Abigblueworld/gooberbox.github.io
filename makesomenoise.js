acbd4d44117dc6d67e79d2af9dc038b9164c1719
BlueWorld/Gruxpin <152799656+Abigblueworld@users.noreply.github.com>
2025-07-22 15:11:56 -0400

    Update makesomenoise.js

--- makesomenoise.js
+++ makesomenoise.js
@@ -1,3 +1,3 @@
 function Makesound(){
-  document.queryselector.innerHTML = "<audio src="https://tcrf.net/images/b/b1/BluesABCTime-room-4-digi-12.ogg" autoplay></audio>"
+  document.queryselectorbyID("#soundnoise").innerHTML = "https://tcrf.net/images/b/b1/BluesABCTime-room-4-digi-12.ogg"
   }
